df84353ce017ca0a53988bd63e7c925b17c80fe7
Glenn <HKAGELE@emory.edu>
2021-10-13 14:02:14 -0400

    add event study objects

--- Scripts/TYP_Analysis.R
+++ Scripts/TYP_Analysis.R
@@ -18,7 +18,7 @@ Physician_Data <- read_rds(paste0(created_data_path,"Physician_Data.rds"))
 # Event Studies for Entire Sample of Doctors ------------------------------------------------------
 # Event Study for Continuous Working Variable
 event_reg <- felm(phys_working ~ rel_m6 + rel_m5 + rel_m4 + rel_m3 + rel_m2 + rel_0 + rel_p1 + rel_p2 + rel_p3 + 
-                  rel_p4 + rel_p5 + rel_p6 + avg_beds + avg_oper_days |year + DocNPI,
+                  rel_p4 + rel_p5 + rel_p6 + female + experience + avg_beds + avg_oper_days |year,
                   data=Physician_Data)
 
 event_reg_coef <- as_tibble(event_reg$coefficients, rownames="term") %>%
@@ -26,7 +26,7 @@ event_reg_coef <- as_tibble(event_reg$coefficients, rownames="term") %>%
                      "rel_p2", "rel_p3", "rel_p4", "rel_p5", "rel_p6")) %>%
   dplyr::rename(estimate=phys_working)
 
-event_reg_ci <- as_tibble(confint(event_reg), rownames="term") %>%
+event_reg_ci <- as_tibble(confint.default(event_reg), rownames="term") %>%
   filter(term %in% c("rel_m6", "rel_m5", "rel_m4", "rel_m3", "rel_m2", "rel_0", "rel_p1",
                      "rel_p2", "rel_p3", "rel_p4", "rel_p5", "rel_p6")) %>%
   dplyr::rename(conf.low='2.5 %', conf.high='97.5 %')
@@ -40,11 +40,10 @@ event_plot_table <- event_reg_coef %>%
   arrange(rel_year)
 
 event_plot <- dwplot(event_plot_table, vline=geom_vline(xintercept=0, linetype=2),
-                     order_vars=c("rel_p6", "rel_p5", "rel_p4", "rel_p3", "rel_p2", "rel_p1", "rel_0",
-                                  "rel_m1", "rel_m2", "rel_m3", "rel_m4", "rel_m5", "rel_m6"),
                      whisker_args=list(color="black", size=1.1),
+                     vars_order =c("rel_p6", "rel_p5", "rel_p4","rel_p3","rel_p2","rel_p1","rel_0","rel_m1","rel_m2","rel_m3","rel_m4","rel_m5","rel_m6"),
                      dot_args=list(color="black")) +
-  coord_flip() + theme_bw() + theme(legend.position="none") + labs(y="Year", x="Estimate and 95% CI") +
+  coord_flip() + theme_bw() + theme(legend.position="none") + labs(y="\nEvent Time\n", x="\nEstimate and 95% CI\n") +
   scale_y_discrete(labels=c("rel_p6"="t+6",
                             "rel_p5"="t+5",
                             "rel_p4"="t+4",
@@ -57,10 +56,9 @@ event_plot <- dwplot(event_plot_table, vline=geom_vline(xintercept=0, linetype=2
                             "rel_m3"="t-3",
                             "rel_m4"="t-4",
                             "rel_m5"="t-5",
-                            "rel_m6"="t-6"))
+                            "rel_m6"="t-6")) + ggtitle("\nDependent Variable: Total Patients\n")
 event_plot
-
-stargazer(event_reg)
+ggsave("Objects/cont_ES_fullsample.pdf")
 
 
 # Event Study for Indicator Working Variable
@@ -73,7 +71,7 @@ event_reg_coef_ind <- as_tibble(event_reg_ind$coefficients, rownames="term") %>%
                      "rel_p2", "rel_p3", "rel_p4", "rel_p5", "rel_p6")) %>%
   dplyr::rename(estimate=working_ind)
 
-event_reg_ci_ind <- as_tibble(confint(event_reg_ind), rownames="term") %>%
+event_reg_ci_ind <- as_tibble(confint.default(event_reg_ind), rownames="term") %>%
   filter(term %in% c("rel_m6", "rel_m5", "rel_m4", "rel_m3", "rel_m2", "rel_0", "rel_p1",
                      "rel_p2", "rel_p3", "rel_p4", "rel_p5", "rel_p6")) %>%
   dplyr::rename(conf.low='2.5 %', conf.high='97.5 %')
@@ -85,11 +83,11 @@ event_plot_ind_table <- event_reg_coef_ind %>%
   arrange(rel_year)
 
 event_plot_ind <- dwplot(event_plot_ind_table, vline=geom_vline(xintercept=0, linetype=2),
-                     order_vars=c("rel_p6", "rel_p5", "rel_p4", "rel_p3", "rel_p2", "rel_p1", "rel_0",
+                     vars_order=c("rel_p6", "rel_p5", "rel_p4", "rel_p3", "rel_p2", "rel_p1", "rel_0",
                                   "rel_m1", "rel_m2", "rel_m3", "rel_m4", "rel_m5", "rel_m6"),
                      whisker_args=list(color="black", size=1.1),
                      dot_args=list(color="black")) +
-  coord_flip() + theme_bw() + theme(legend.position="none") + labs(y="Year", x="Estimate and 95% CI") +
+  coord_flip() + theme_bw() + theme(legend.position="none") + labs(y="\nEvent Time\n", x="\nEstimate and 95% CI\n") +
   scale_y_discrete(labels=c("rel_p6"="t+6",
                             "rel_p5"="t+5",
                             "rel_p4"="t+4",
@@ -102,13 +100,14 @@ event_plot_ind <- dwplot(event_plot_ind_table, vline=geom_vline(xintercept=0, li
                             "rel_m3"="t-3",
                             "rel_m4"="t-4",
                             "rel_m5"="t-5",
-                            "rel_m6"="t-6"))
+                            "rel_m6"="t-6")) + ggtitle("\nDependent Variable: Indicator for Working\n")
 event_plot_ind
+ggsave("Objects/ind_ES_fullsample.pdf")
 
 # Repeat Event Studies for Old Doctors ----------------------------------------------------------------
 # Event Study for Continuous Working Variable (Old Doctors)
 event_reg_old <- felm(phys_working ~ rel_m6 + rel_m5 + rel_m4 + rel_m3 + rel_m2 + rel_0 + rel_p1 + rel_p2 + rel_p3 + 
-                    rel_p4 + rel_p5 + rel_p6 + experience + female + avg_beds + avg_oper_days | year,
+                    rel_p4 + rel_p5 + rel_p6 + female + avg_beds + avg_oper_days | year,
                   data=filter(Physician_Data, experience>=40))
 
 event_reg_coef_old <- as_tibble(event_reg_old$coefficients, rownames="term") %>%
@@ -116,7 +115,7 @@ event_reg_coef_old <- as_tibble(event_reg_old$coefficients, rownames="term") %>%
                      "rel_p2", "rel_p3", "rel_p4", "rel_p5", "rel_p6")) %>%
   dplyr::rename(estimate=phys_working)
 
-event_reg_ci_old <- as_tibble(confint(event_reg_old), rownames="term") %>%
+event_reg_ci_old <- as_tibble(confint.default(event_reg_old), rownames="term") %>%
   filter(term %in% c("rel_m6", "rel_m5", "rel_m4", "rel_m3", "rel_m2", "rel_0", "rel_p1",
                      "rel_p2", "rel_p3", "rel_p4", "rel_p5", "rel_p6")) %>%
   dplyr::rename(conf.low='2.5 %', conf.high='97.5 %')
@@ -130,11 +129,11 @@ event_plot_table_old <- event_reg_coef_old %>%
   arrange(rel_year)
 
 event_plot_old <- dwplot(event_plot_table_old, vline=geom_vline(xintercept=0, linetype=2),
-                     order_vars=c("rel_p6", "rel_p5", "rel_p4", "rel_p3", "rel_p2", "rel_p1", "rel_0",
+                     vars_order =c("rel_p6", "rel_p5", "rel_p4", "rel_p3", "rel_p2", "rel_p1", "rel_0",
                                   "rel_m1", "rel_m2", "rel_m3", "rel_m4", "rel_m5", "rel_m6"),
                      whisker_args=list(color="black", size=1.1),
                      dot_args=list(color="black")) +
-  coord_flip() + theme_bw() + theme(legend.position="none") + labs(y="Year", x="Estimate and 95% CI") +
+  coord_flip() + theme_bw() + theme(legend.position="none") + labs(y="\nEvent Time\n", x="\nEstimate and 95% CI\n") +
   scale_y_discrete(labels=c("rel_p6"="t+6",
                             "rel_p5"="t+5",
                             "rel_p4"="t+4",
@@ -147,13 +146,13 @@ event_plot_old <- dwplot(event_plot_table_old, vline=geom_vline(xintercept=0, li
                             "rel_m3"="t-3",
                             "rel_m4"="t-4",
                             "rel_m5"="t-5",
-                            "rel_m6"="t-6"))
+                            "rel_m6"="t-6")) + ggtitle("\nDependent Variable: Total Patients (Senior Physician Sample)\n")
 event_plot_old
-
+ggsave("Objects/cont_ES_oldsample.pdf")
 
 # Event Study for Indicator Working Variable (Old Doctors)
 event_reg_ind_old <- felm(working_ind ~ rel_m6 + rel_m5 + rel_m4 + rel_m3 + rel_m2 + rel_0 + rel_p1 + rel_p2 + rel_p3 + 
-                        rel_p4 + rel_p5 + rel_p6 + experience + female + avg_beds + avg_oper_days | year,
+                        rel_p4 + rel_p5 + rel_p6 + female + avg_beds + avg_oper_days | year,
                       data=filter(Physician_Data, experience>=40))
 
 event_reg_coef_ind_old <- as_tibble(event_reg_ind_old$coefficients, rownames="term") %>%
@@ -161,7 +160,7 @@ event_reg_coef_ind_old <- as_tibble(event_reg_ind_old$coefficients, rownames="te
                      "rel_p2", "rel_p3", "rel_p4", "rel_p5", "rel_p6")) %>%
   dplyr::rename(estimate=working_ind)
 
-event_reg_ci_ind_old<- as_tibble(confint(event_reg_ind_old), rownames="term") %>%
+event_reg_ci_ind_old<- as_tibble(confint.default(event_reg_ind_old), rownames="term") %>%
   filter(term %in% c("rel_m6", "rel_m5", "rel_m4", "rel_m3", "rel_m2", "rel_0", "rel_p1",
                      "rel_p2", "rel_p3", "rel_p4", "rel_p5", "rel_p6")) %>%
   dplyr::rename(conf.low='2.5 %', conf.high='97.5 %')
@@ -173,11 +172,11 @@ event_plot_ind_table_old <- event_reg_coef_ind_old %>%
   arrange(rel_year)
 
 event_plot_ind_old <- dwplot(event_plot_ind_table_old, vline=geom_vline(xintercept=0, linetype=2),
-                         order_vars=c("rel_p6", "rel_p5", "rel_p4", "rel_p3", "rel_p2", "rel_p1", "rel_0",
+                         vars_order=c("rel_p6", "rel_p5", "rel_p4", "rel_p3", "rel_p2", "rel_p1", "rel_0",
                                       "rel_m1", "rel_m2", "rel_m3", "rel_m4", "rel_m5", "rel_m6"),
                          whisker_args=list(color="black", size=1.1),
                          dot_args=list(color="black")) +
-  coord_flip() + theme_bw() + theme(legend.position="none") + labs(y="Year", x="Estimate and 95% CI") +
+  coord_flip() + theme_bw() + theme(legend.position="none") + labs(y="\nEvent Time\n", x="\nEstimate and 95% CI\n") +
   scale_y_discrete(labels=c("rel_p6"="t+6",
                             "rel_p5"="t+5",
                             "rel_p4"="t+4",
@@ -190,8 +189,110 @@ event_plot_ind_old <- dwplot(event_plot_ind_table_old, vline=geom_vline(xinterce
                             "rel_m3"="t-3",
                             "rel_m4"="t-4",
                             "rel_m5"="t-5",
-                            "rel_m6"="t-6"))
+                            "rel_m6"="t-6")) + ggtitle("\nDependent Variable: Indicator for Working (Senior Physician Sample)\n")
 event_plot_ind_old
+ggsave("Objects/ind_ES_oldsample.pdf")
+
+
+# Event Studies for Sample of Doctors excluding 2009 treatment ------------------------------------------------------
+event_reg_subset <- felm(phys_working ~ rel_m5 + rel_m4 + rel_m3 + rel_m2 + rel_0 + rel_p1 + rel_p2 + rel_p3 + 
+                    rel_p4 + rel_p5 + female + experience + avg_beds + avg_oper_days |year,
+                  data=filter(Physician_Data,minyr_EHR>2009))
+
+event_reg_coef_subset <- as_tibble(event_reg_subset$coefficients, rownames="term") %>%
+  filter(term %in% c("rel_m5", "rel_m4", "rel_m3", "rel_m2", "rel_0", "rel_p1",
+                     "rel_p2", "rel_p3", "rel_p4", "rel_p5")) %>%
+  dplyr::rename(estimate=phys_working)
+
+event_reg_ci_subset <- as_tibble(confint.default(event_reg_subset), rownames="term") %>%
+  filter(term %in% c("rel_m5", "rel_m4", "rel_m3", "rel_m2", "rel_0", "rel_p1",
+                     "rel_p2", "rel_p3", "rel_p4", "rel_p5")) %>%
+  dplyr::rename(conf.low='2.5 %', conf.high='97.5 %')
+
+extra_row <- tibble(term="rel_m1", estimate=0, conf.low=0, conf.high=0, rel_year=-1)
+
+event_plot_table_subset <- event_reg_coef_subset %>%
+  left_join(event_reg_ci_subset, by="term") %>%
+  mutate(rel_year=c(-5,-4,-3,-2,0,1,2,3,4,5)) %>%
+  bind_rows(extra_row) %>%
+  arrange(rel_year)
+
+event_plot_subset <- dwplot(event_plot_table_subset, vline=geom_vline(xintercept=0, linetype=2),
+                     whisker_args=list(color="black", size=1.1),
+                     vars_order =c("rel_p5", "rel_p4","rel_p3","rel_p2","rel_p1","rel_0","rel_m1","rel_m2","rel_m3","rel_m4","rel_m5"),
+                     dot_args=list(color="black")) +
+  coord_flip() + theme_bw() + theme(legend.position="none") + labs(y="\nEvent Time\n", x="\nEstimate and 95% CI\n") +
+  scale_y_discrete(labels=c("rel_p5"="t+5",
+                            "rel_p4"="t+4",
+                            "rel_p3"="t+3",
+                            "rel_p2"="t+2",
+                            "rel_p1"="t+1",
+                            "rel_0"="0",
+                            "rel_m1"="t-1",
+                            "rel_m2"="t-2",
+                            "rel_m3"="t-3",
+                            "rel_m4"="t-4",
+                            "rel_m5"="t-5")) + ggtitle("\nDependent Variable: Total Patients (Exclude treated in 2009)\n")
+event_plot_subset
+ggsave("Objects/cont_ES_subset2009.pdf")
+
+
+# Event Study for Indicator Working Variable
+event_reg_ind_subset <- felm(working_ind ~ rel_m5 + rel_m4 + rel_m3 + rel_m2 + rel_0 + rel_p1 + rel_p2 + rel_p3 + 
+                        rel_p4 + rel_p5 + experience + female + avg_beds + avg_oper_days | year,
+                      data=filter(Physician_Data, minyr_EHR>2009))
+
+event_reg_coef_ind_subset <- as_tibble(event_reg_ind_subset$coefficients, rownames="term") %>%
+  filter(term %in% c("rel_m5", "rel_m4", "rel_m3", "rel_m2", "rel_0", "rel_p1",
+                     "rel_p2", "rel_p3", "rel_p4", "rel_p5")) %>%
+  dplyr::rename(estimate=working_ind)
+
+event_reg_ci_ind_subset <- as_tibble(confint.default(event_reg_ind_subset), rownames="term") %>%
+  filter(term %in% c( "rel_m5", "rel_m4", "rel_m3", "rel_m2", "rel_0", "rel_p1",
+                     "rel_p2", "rel_p3", "rel_p4", "rel_p5")) %>%
+  dplyr::rename(conf.low='2.5 %', conf.high='97.5 %')
+
+event_plot_ind_table_subset <- event_reg_coef_ind_subset %>%
+  left_join(event_reg_ci_ind_subset, by="term") %>%
+  mutate(rel_year=c(-5,-4,-3,-2,0,1,2,3,4,5)) %>%
+  bind_rows(extra_row) %>%
+  arrange(rel_year)
+
+event_plot_ind <- dwplot(event_plot_ind_table_subset, vline=geom_vline(xintercept=0, linetype=2),
+                         vars_order=c("rel_p5", "rel_p4", "rel_p3", "rel_p2", "rel_p1", "rel_0",
+                                      "rel_m1", "rel_m2", "rel_m3", "rel_m4", "rel_m5"),
+                         whisker_args=list(color="black", size=1.1),
+                         dot_args=list(color="black")) +
+  coord_flip() + theme_bw() + theme(legend.position="none") + labs(y="\nEvent Time\n", x="\nEstimate and 95% CI\n") +
+  scale_y_discrete(labels=c("rel_p5"="t+5",
+                            "rel_p4"="t+4",
+                            "rel_p3"="t+3",
+                            "rel_p2"="t+2",
+                            "rel_p1"="t+1",
+                            "rel_0"="0",
+                            "rel_m1"="t-1",
+                            "rel_m2"="t-2",
+                            "rel_m3"="t-3",
+                            "rel_m4"="t-4",
+                            "rel_m5"="t-5")) + ggtitle("\nDependent Variable: Indicator for Working (Exclude treated in 2009)\n")
+event_plot_ind
+ggsave("Objects/ind_ES_subset2009.pdf")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
