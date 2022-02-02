5be654de3fdbe0d0fc850776c470638bd42cae7b
Glenn <HKAGELE@emory.edu>
2022-02-02 12:19:09 -0500

    edit scripts during analysis

--- Scripts/analysis.R
+++ Scripts/analysis.R
@@ -1,19 +1,148 @@
 library(did)
+library(dplyr)
 
-no_retire_data <- Physician_Data %>%
-  dplyr::filter(ever_retire==0)
+# ------------------------------------- ANALYSIS  ------------------------------------
+#                                       Hanna Glenn, Emory University
+#                                       1/31/2022
 
+# This script reads in "Physician_Data.rds" from data5, the final dataset used in my third year paper. 
+# The analysis in this script focuses mainly on Pedro Sant'Anna and Brent Callaway's difference in
+# difference methodologies. 
 
-out <- att_gt(yname = "hosp_patient_count",
+# Retirement ---------------------------------------------------------------------------
+
+# Full sample (exclude 2017 because I cannot observe retirement. Also think about whether to exclude 2016)
+retire_es <- att_gt(yname = "retire_lowclaims",
               gname = "minyr_EHR",
               idname = "DocNPI",
               tname = "year",
               xformla = ~age,
-              data = no_retire_data,
+              data = dplyr::filter(Physician_Data,year<2017),
               est_method = "reg",
               control_group = "notyettreated"
 )
-ggdid(out)
+ggdid(retire_es)
 
-out_dyn <- aggte(out, type = "dynamic",balance_e=1)
-ggdid(out_dyn)
+retire_es_dyn <- aggte(retire_es, type = "dynamic")
+ggdid(retire_es_dyn)
+
+
+
+# Older Physicians
+old_retire_es <- att_gt(yname = "retire_lowclaims",
+                    gname = "minyr_EHR",
+                    idname = "DocNPI",
+                    tname = "year",
+                    xformla = ~age,
+                    data = dplyr::filter(Physician_Data,year<2017 & age>50),
+                    est_method = "reg",
+                    control_group = "notyettreated"
+)
+ggdid(old_retire_es)
+
+old_retire_es_dyn <- aggte(old_retire_es, type = "dynamic")
+ggdid(old_retire_es_dyn)
+
+# Office- based outcomes -------------------------------------------------------------
+
+# Full Sample using fraction of patients in office
+office_frac_es <- att_gt(yname = "pos_office",
+                    gname = "minyr_EHR",
+                    idname = "DocNPI",
+                    tname = "year",
+                    xformla = ~age,
+                    data = dplyr::filter(Physician_Data,ever_retire==0),
+                    est_method = "reg",
+                    control_group = "notyettreated"
+)
+
+ggdid(office_frac_es)
+
+office_frac_es_dyn <- aggte(office_frac_es, type = "dynamic")
+ggdid(office_frac_es_dyn)
+
+# Old sample using fraction of claims in office
+old_office_frac_es <- att_gt(yname = "pos_office",
+                        gname = "minyr_EHR",
+                        idname = "DocNPI",
+                        tname = "year",
+                        xformla = ~age,
+                        data = dplyr::filter(Physician_Data,age>50 & ever_retire==0),
+                        est_method = "reg",
+                        control_group = "notyettreated"
+)
+
+old_office_frac_es_dyn <- aggte(old_office_frac_es, type = "dynamic")
+ggdid(old_office_frac_es_dyn)
+
+# Full Sample using indicator for working in office
+office_ind_es <- att_gt(yname = "work_in_office",
+                        gname = "minyr_EHR",
+                        idname = "DocNPI",
+                        tname = "year",
+                        xformla = ~age,
+                        data = dplyr::filter(Physician_Data,ever_retire==0),
+                        est_method = "reg",
+                        control_group = "notyettreated"
+)
+
+ggdid(office_ind_es)
+
+office_ind_es_dyn <- aggte(office_ind_es, type = "dynamic")
+ggdid(office_ind_es_dyn)
+
+# Old Sample using indicator for working in office
+old_office_ind_es <- att_gt(yname = "work_in_office",
+                        gname = "minyr_EHR",
+                        idname = "DocNPI",
+                        tname = "year",
+                        xformla = ~age,
+                        data = dplyr::filter(Physician_Data,ever_retire==0 & age>50),
+                        est_method = "reg",
+                        control_group = "notyettreated"
+)
+
+ggdid(old_office_ind_es)
+
+old_office_ind_es_dyn <- aggte(old_office_ind_es, type = "dynamic")
+ggdid(old_office_ind_es_dyn)
+
+
+## PRODUCTIVITY ####
+
+# Full sample total claim count
+claim_es <- att_gt(yname = "claim_count_total",
+                            gname = "minyr_EHR",
+                            idname = "DocNPI",
+                            tname = "year",
+                            xformla = ~age,
+                            data = dplyr::filter(Physician_Data,ever_retire==0 & ever_work_in_office==0),
+                            est_method = "reg",
+                            control_group = "notyettreated"
+)
+
+ggdid(claim_es)
+
+claim_es_dyn <- aggte(claim_es, type = "dynamic")
+ggdid(claim_es_dyn)
+
+# Old sample total claim count
+old_claim_es <- att_gt(yname = "claim_count_total",
+                   gname = "minyr_EHR",
+                   idname = "DocNPI",
+                   tname = "year",
+                   xformla = ~age,
+                   data = dplyr::filter(Physician_Data,ever_retire==0 & ever_work_in_office==0 & age>50),
+                   est_method = "reg",
+                   control_group = "notyettreated"
+)
+
+ggdid(old_claim_es)
+
+old_claim_es_dyn <- aggte(old_claim_es, type = "dynamic")
+ggdid(old_claim_es_dyn)
+
+
+
+
+
--- Scripts/data5_MDPPAS.R
+++ Scripts/data5_MDPPAS.R
@@ -21,23 +21,34 @@ for (i in 2009:2017) {
 MDPPAS <- rbind(MDPPAS2009, MDPPAS2010, MDPPAS2011, MDPPAS2012, MDPPAS2013, MDPPAS2014, MDPPAS2015, MDPPAS2016, MDPPAS2017)
 
 MDPPAS <- MDPPAS %>%
-  dplyr::select(-name_middle, -spec_broad, -spec_prim_1, -spec_prim_2,
+  dplyr::select(-name_middle, -name_last, -name_first, -spec_broad, -spec_prim_1, -spec_prim_2,
                 -phy_zip7, -claim_count7, -phy_zip8, -claim_count8, -phy_zip9, -claim_count9,
                 -phy_zip10, -claim_count10, -phy_zip11, -claim_count11, -phy_zip12, 
-                -claim_count12)
+                -claim_count12, -tin1_legal_name, -tin2_legal_name)
 
 # Read in Aggregated Pairs data from "data4_physician_level.R"
 Aggregated_Pairs <- read_rds(paste0(created_data_path,"Aggregated_Pairs.rds"))
 
 Aggregated_Pairs <- complete(Aggregated_Pairs,DocNPI,year=2009:2017)
 
+# fill time invariant variables 
+Aggregated_Pairs <- Aggregated_Pairs %>%
+  dplyr::group_by(DocNPI) %>%
+  tidyr::fill(grad_year,.direction="downup") %>%
+  tidyr::fill(minyr_EHR,.direction="downup") %>%
+  tidyr::fill(anyEHR_exposed,.direction="down") %>%
+  tidyr::fill(num_hospitals_constant,.direction="downup") %>%
+  tidyr::fill(num_systems,.direction="downup") %>%
+  tidyr::fill(exposed_ever,.direction="downup") %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(experience=ifelse(is.na(experience),year-grad_year,experience))
+  
+
 # Merge MDPPAS to main dataset and fill time invariant variables
 Physician_Data <- Aggregated_Pairs %>%
   dplyr::left_join(MDPPAS, by=c("DocNPI"="npi", "year"="Year")) %>%
   dplyr::group_by(DocNPI) %>%
   tidyr::fill(sex, .direction="downup") %>%
-  tidyr::fill(name_last, .direction="downup") %>%
-  tidyr::fill(name_first, .direction="downup") %>%
   tidyr::fill(birth_dt, .direction="downup") %>%
   dplyr::ungroup() %>%
   dplyr::mutate(female=ifelse(sex=='F',1,0)) 
@@ -49,6 +60,11 @@ Physician_Data <- Physician_Data %>%
   dplyr::mutate(birth_year=as.numeric(birth_year)) %>%
   dplyr::mutate(age=year-birth_year)
 
+# Drop any physicians with less than 30% of patients in hospitals
+Physician_Data <- Physician_Data %>%
+  dplyr::group_by(DocNPI) %>%
+  dplyr::mutate(max=max(pos_inpat,na.rm=T)) %>%
+  dplyr::filter(max>.3)
 
 # CREATE DEPENDENT VARIABLES -----------------------------------------------------------------------------------------
 
@@ -60,27 +76,35 @@ Physician_Data <- Physician_Data %>%
   dplyr::mutate(claim_count_total=ifelse(is.na(claim_count1),NA,sum(dplyr::c_across(tidyr::starts_with("claim_count")),na.rm=T))) %>%
   dplyr::ungroup()
 
+# Remove those who only appear in the data for one year 
+Physician_Data <- Physician_Data %>%
+  dplyr::mutate(pos=ifelse(is.na(claim_count_total),0,1)) %>%
+  dplyr::group_by(DocNPI) %>%
+  dplyr::mutate(sum=sum(pos)) %>%
+  dplyr::ungroup() %>%
+  dplyr::filter(sum>1) %>%
+  dplyr::select(-pos,-sum)
+
 # Create a variable that sums up the claim count in all future years
 Physician_Data <- Physician_Data %>%
   dplyr::group_by(DocNPI) %>%
-  dplyr::mutate(future_claims=ifelse(year==2009,sum(claim_count_total[year>2009],na.rm=T),NA)) %>%
-  dplyr::mutate(future_patients=ifelse(year==2009,sum(hosp_patient_count[year>2009],na.rm=T),NA))
+  dplyr::mutate(future_claims=ifelse(year==2009,sum(claim_count_total[year>2009],na.rm=T),NA))
 
 for (i in 2010:2017){
   Physician_Data <- Physician_Data %>%
     dplyr::group_by(DocNPI) %>%
-    dplyr::mutate(future_claims=ifelse(year==i,sum(claim_count_total[year>i],na.rm=T),future_claims)) %>%
-    dplyr::mutate(future_patients=ifelse(year==i,sum(hosp_patient_count[year>i],na.rm=T),future_patients))
+    dplyr::mutate(future_claims=ifelse(year==i,sum(claim_count_total[year>i],na.rm=T),future_claims)) 
 } 
 
-# I only consider it retirement if all future patients AND claims are zero. Create this variable
+# I only consider it retirement if all future claims are zero. Create this variable
 Physician_Data <- Physician_Data %>% dplyr::ungroup() %>%
-  dplyr::mutate(retire=ifelse(year<2015 & future_claims==0 & future_patients==0,1,0)) %>%
-  dplyr::mutate(retire_2014=ifelse(year==2014 & retire==1,1,NA)) %>%
+  dplyr::mutate(retire=ifelse(future_claims==0,1,0)) %>%
+  dplyr::mutate(retire_2016=ifelse(year==2016 & retire==1,1,NA)) %>%
   dplyr::group_by(DocNPI) %>%
-  tidyr::fill(retire_2014,.direction="downup") %>%
+  tidyr::fill(retire_2016,.direction="downup") %>%
   dplyr::ungroup() %>%
-  dplyr::mutate(retire=ifelse(year==2015 & retire_2014==1,1,retire))
+  dplyr::mutate(retire=ifelse(year==2017 & retire_2016==1,1,retire)) %>%
+  dplyr::mutate(retire=ifelse(year==2017 & is.na(retire_2016),0,retire))
 
 # This variable creates the retirement indicator one year too early. I want the first year of retirement to be the first year showing zeros.
 # Fix this
@@ -88,13 +112,15 @@ minyr_retire <- Physician_Data %>%
   dplyr::filter(retire==1) %>%
   dplyr::group_by(DocNPI) %>%
   dplyr::mutate(minyr_retire=min(year)) %>%
-  dplyr::distinct(DocNPI, minyr_retire)
+  dplyr::distinct(DocNPI, minyr_retire) %>%
+  dplyr::ungroup()
 
 Physician_Data <- Physician_Data %>%
   dplyr::left_join(minyr_retire, by="DocNPI")
 
 Physician_Data <- Physician_Data %>%
-  dplyr::mutate(retire=ifelse(year==minyr_retire,0,retire))
+  dplyr::mutate(retire=ifelse(year==minyr_retire,0,retire)) %>%
+  dplyr::mutate(retire=ifelse(is.na(retire),0,retire))
 
 # Create variable for whether the physician ever retires
 Physician_Data <- Physician_Data %>% dplyr::ungroup() %>%
@@ -104,13 +130,45 @@ observe <- Physician_Data %>%
   dplyr::filter(ever_retire==1) %>%
   dplyr::select(DocNPI, year,age,retire,hosp_patient_count,claim_count_total)
 
-# Now that the retirement variable is created I no longer need 2016 and 2017 years
+# Consider looking at those physicians who have low claim counts (barely working)
 Physician_Data <- Physician_Data %>%
-  dplyr::filter(year<2016) %>%
-  dplyr::select(-retire_2014)
+  dplyr::mutate(high_claim=ifelse(claim_count_total>200,1,0)) %>%
+  dplyr::group_by(DocNPI) %>%
+  dplyr::mutate(future_high_claims=ifelse(year==2009,sum(high_claim[year>2009],na.rm=T),NA))
+
+for (i in 2010:2017){
+  Physician_Data <- Physician_Data %>%
+    dplyr::group_by(DocNPI) %>%
+    dplyr::mutate(future_high_claims=ifelse(year==i,sum(high_claim[year>i],na.rm=T),future_high_claims)) 
+} 
+
+Physician_Data <- Physician_Data %>% dplyr::ungroup() %>%
+  dplyr::mutate(retire_lowclaims=ifelse(future_high_claims==0,1,0)) %>%
+  dplyr::mutate(retire_low2016=ifelse(year==2016 & retire_lowclaims==1,1,NA)) %>%
+  dplyr::group_by(DocNPI) %>%
+  tidyr::fill(retire_low2016,.direction="downup") %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(retire_lowclaims=ifelse(year==2017 & retire_low2016==1,1,retire_lowclaims)) %>%
+  dplyr::mutate(retire_lowclaims=ifelse(year==2017 & is.na(retire_low2016),0,retire_lowclaims))
+
+minyr_retire_lowclaims <- Physician_Data %>%
+  dplyr::filter(retire_lowclaims==1) %>%
+  dplyr::group_by(DocNPI) %>%
+  dplyr::mutate(minyr_retire_lowclaims=min(year)) %>%
+  dplyr::distinct(DocNPI, minyr_retire_lowclaims) %>%
+  dplyr::ungroup()
 
 Physician_Data <- Physician_Data %>%
-  dplyr::mutate(retire=ifelse(is.na(retire),0,retire))
+  dplyr::left_join(minyr_retire_lowclaims, by="DocNPI")
+
+Physician_Data <- Physician_Data %>%
+  dplyr::mutate(retire_lowclaims=ifelse(year==minyr_retire_lowclaims,0,retire_lowclaims)) %>%
+  dplyr::mutate(retire_lowclaims=ifelse(is.na(retire_lowclaims),0,retire_lowclaims))
+
+
+Physician_Data <- Physician_Data %>% dplyr::ungroup() %>%
+  dplyr::mutate(ever_retire_lowclaims=ifelse(is.na(minyr_retire_lowclaims),0,1))
+
 
 
 # OFFICE ####
@@ -126,17 +184,10 @@ observe <- Physician_Data %>%
   dplyr::filter(sum>0) %>%
   dplyr::select(DocNPI,year,hosp_patient_count,claim_count_total)
 
-# I'm thinking to replace all NAs with 0 if hospital patient count is zero. This will give a lower bound of claim count
+# I'm thinking to replace all NAs with 0.
 Physician_Data <- Physician_Data %>%
-  dplyr::mutate(claim_count_total=ifelse(is.na(claim_count_total) & hosp_patient_count==0,0,claim_count_total))
-
-# Replace with 10 if hosp count is positive
-Physician_Data <- Physician_Data %>%
-  dplyr::mutate(claim_count_total=ifelse(is.na(claim_count_total) & hosp_patient_count>0,10,claim_count_total))
-
-# Change office fraction to 0 if claim count is 0
-Physician_Data <- Physician_Data %>%
-  dplyr::mutate(pos_office=ifelse(is.na(pos_office) & claim_count_total==0,0,pos_office))
+  dplyr::mutate(claim_count_total=ifelse(is.na(claim_count_total),0,claim_count_total)) %>%
+  dplyr::mutate(pos_office=ifelse(is.na(pos_office),0,pos_office))
 
 # Since there is already a variable of fraction of claims in office setting, I don't need to do much. 
 # I'll create an indicator or have positive patients in office setting to capture different variation. 
@@ -147,13 +198,26 @@ Physician_Data <- Physician_Data %>%
 Physician_Data <-Physician_Data %>%
   dplyr::mutate(work_in_hosp=ifelse(pos_inpat>0,1,0))
 
+# Create a variable for whether the physician ever works in an office
+Physician_Data <- Physician_Data %>%
+  dplyr::group_by(DocNPI) %>%
+  dplyr::mutate(sum=sum(work_in_office)) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(ever_work_in_office=ifelse(sum>0,1,0)) 
 
 
 
 # PRODUCTIVITY ####
 
 # For productivity, I will simply use claim count and patient count as the dependent variable.
-# I may need to do additional cleaning of the claim variable to code some of the missings if I can do so reasonably
+# The key for this variable is to limit the sample appropriately. 
+
+
+# ZIP CODES ####
+Physician_Data <- Physician_Data %>% dplyr::ungroup() %>%
+  dplyr::mutate(multiple_zip=ifelse(is.na(phy_zip2),0,1))
+
+
 
 
 
@@ -168,9 +232,8 @@ balance_check <- Physician_Data %>%
 
 is.pbalanced(balance_check)
 
-observe <- Physician_Data %>%
-   dplyr::filter(DocNPI==1023000000)
-
+data <- Physician_Data %>%
+  dplyr::select(-future_patients, -spec_prim_1_name, -spec_prim_2_name, -name_last,-name_first, -sex, -birth_dt)
   
   
   
